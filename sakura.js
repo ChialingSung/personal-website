1106d0ab7041ec39089d0af28e5f8853d6ded727
Chialing Sung <sungcl@bu.edu>
2025-03-28 12:33:07 -0400

    Initial commit

---
+++ sakura.js
@@ -0,0 +1,13 @@
+new Sakura('body', {
+  colors: [
+    {
+      gradientColorStart: '#ffd1dc',
+      gradientColorEnd: '#ff9eb5',
+      gradientColorDegree: 120
+    }
+  ],
+  speed: 1.5,
+  maxSize: 12,
+  minSize: 8,
+  fallSpeed: 1.2
+});
